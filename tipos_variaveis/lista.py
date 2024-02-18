3ef1350c79354275105085aaa2cb66e679e10cd0
Robson Santos <rboxe68@gmail.com>
2024-02-17 22:45:39 -0300

    Atualizado

---
+++ tipos_variaveis/lista.py
@@ -0,0 +1,45 @@
+# Declaração
+
+# minha_lista = [1, 2, 3, 4, 5, "rocketseat", True, False]
+minha_lista = [1, 10, 3, 3, 5, 6, 9, 8, 7, 112]
+
+# Exibindo a lista
+print("Minha lista de exemplo", minha_lista)
+
+# minha_lista[0] = "python"
+print("Minha lista de exemplo", minha_lista)
+
+print("minha_lista[0]:", minha_lista[0])
+print("minha_lista[5]:", minha_lista[5])
+print("minha_lista[1:7]", minha_lista[1:7])
+print("minha_lista[:6]:", minha_lista[:6])
+print("minha_lista[2:]:", minha_lista[2:])
+
+# Método de lista
+
+# Método append(): Adiciona um elemento ao final da lista
+minha_lista.append(6)
+print("Após append(6):", minha_lista)
+
+# Método index()
+indice = minha_lista.index(6)
+print("Indice do elemento 6:", indice)
+print(indice)
+
+# Método insert(): Insere um elemento em um índice específico
+minha_lista.insert(2, 10)
+print("Apos o insert(2, 10):", minha_lista)
+print(minha_lista)
+
+# Método pop()
+elemento_removido = minha_lista.pop(3)
+print("Elemento removido:", elemento_removido)
+print("Apos pop(3):", minha_lista)
+
+# Método remove()
+minha_lista.remove(True)
+print("Apos remove(True):", minha_lista)
+
+# Método sort()
+minha_lista.sort()
+print("Apos sort():", minha_lista)
